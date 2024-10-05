0482f54dd8e891439d26ab50b36b2d4fa2a9a997
Miguel Gramacho (CTW) <Miguel.Gramacho@ctw.bmwgroup.com>
2024-10-05 01:28:38 +0100

    Test mathematical formulas with markdown

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# fermats-last-theorem
+# What is Fermats Last Theorem About?
+$$
+a^n+b^n=c^n
+$$
---
+++ theorem.py
@@ -0,0 +1,4 @@
+def check_fermats_last_theorem(start, end):
+    if start <= 2:
+        raise ValueError("Your starting point must be greater than 2.")
+
